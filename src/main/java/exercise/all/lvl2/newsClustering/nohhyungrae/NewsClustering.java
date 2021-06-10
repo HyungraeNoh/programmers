77d6d8f65658c3e198fa2c074a3698e24ef97a65
noh.hyungrae <noh.hyungrae@sys2020dv144.home.e-kmall.com>
2021-06-10 09:07:54 +0900

    commit

--- src/main/java/exercise/all/lvl2/newsClustering/nohhyungrae/NewsClustering.java
+++ src/main/java/exercise/all/lvl2/newsClustering/nohhyungrae/NewsClustering.java
@@ -3,6 +3,15 @@ package exercise.all.lvl2.newsClustering.nohhyungrae;
 public class NewsClustering {
 	
 	public static void main(String[] args) {
+		
+		String str1 = "";
+		String str2 = "";
+		System.out.println("solution : "+solution(str1, str2));
 	}
+	
+	public static int solution(String str1, String str2) {
+        int answer = 0;
+        return answer;
+    }
 
 }
