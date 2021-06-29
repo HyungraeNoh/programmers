fb9cadc9aea6f1a698401820a151b47183889383
noh.hyungrae <noh.hyungrae@sys2020dv144.home.e-kmall.com>
2021-06-30 08:58:08 +0900

    카펫 - 마무리

--- src/main/java/exercise/all/lvl2/carpet/nohhyungrae/Carpet.java
+++ src/main/java/exercise/all/lvl2/carpet/nohhyungrae/Carpet.java
@@ -12,45 +12,30 @@ public class Carpet {
 		int brown3 = 24;
 		int yellow3 = 24;
 		
-		System.out.println("solution : "+solution(brown3, yellow3));
+		System.out.println("solution : "+solution(brown, yellow));
 	}
 	
 	public static int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         int sum = brown + yellow;
         
-        for(int width=1;width<=sum/2; width++) {
-        	int len = sum/width;
-        	if(width >=3 && len >= width && len*width == sum) {
-        		System.out.println((len*2)+((width-2)*2));
+        //세로
+        for(int width=3;width<=sum/2; width++) {
+        	int len = sum/width; //가로
+        	if(len >= width && len*width == sum) {
+        		//System.out.println((len*2)+((width-2)*2));
         		if((len*2)+((width-2)*2)== brown) {
         			answer[0] = len;
         			answer[1] = width;
+        			break;
         		}
         	}
         }
-        
-        
+        System.out.println("==============");
+        for(int a : answer) {
+        	System.out.println(a);
+        }
         return answer;
     }
-	
-	public static int[] solution2(int brown, int yellow) {
-		int[] answer = new int[2];
-		int tot = brown + yellow;
-		
-		for(int i=1;i<=tot/2; i++) {
-			int f = tot/i;
-			if(i >=3 && f >= i && f*i == tot) {
-				System.out.println((f*2)+((i-2)*2));
-				if((f*2)+((i-2)*2)== brown) {
-					answer[0] = f;
-					answer[1] = i;
-				}
-			}
-		}
-		
-		
-		return answer;
-	}
 
 }
