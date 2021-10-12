cce6d1fb4b07abb9b7394fdf4dfa49f29a7a90f9
noh.hyungrae <noh.hyungrae@sys2020dv144.home.e-kmall.com>
2021-10-13 08:34:56 +0900

    컬러링북

--- src/main/java/exercise/all/lvl2/coloringBook/nohhyungrae/ColoringBook.java
+++ src/main/java/exercise/all/lvl2/coloringBook/nohhyungrae/ColoringBook.java
@@ -3,6 +3,22 @@ package exercise.all.lvl2.coloringBook.nohhyungrae;
 public class ColoringBook {
 
 	public static void main(String[] args) {
+		int m = 6;
+		int n = 4;
+		int[][] picture = {{1, 1, 1, 0}, {1, 2, 2, 0}, {1, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 3}, {0, 0, 0, 3}};
+		
+		System.out.println("solution : "+solution(m,n,picture));
+		
 	}
+	
+	public static int[] solution(int m, int n, int[][] picture) {
+        int numberOfArea = 0;
+        int maxSizeOfOneArea = 0;
+
+        int[] answer = new int[2];
+        answer[0] = numberOfArea;
+        answer[1] = maxSizeOfOneArea;
+        return answer;
+    }
 
 }
